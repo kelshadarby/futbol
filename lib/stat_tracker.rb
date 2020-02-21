59bd530be1568147931f4b60324838dd5f46e433
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-21 08:59:33 -0700

    Start on best and worst fans methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,4 +11,35 @@ class StatTracker
     @teams = Team.create_teams(team_path)
     @game_teams = GameTeam.create_game_teams(game_teams_path)
   end
+
+
+  def best_fans
+  end
+
+  def home_games(team_id)
+    @game_teams.find_all do |game_team|
+      game_team.hoa == "home" && game_team.team_id == team_id
+    end
+  end
+
+  def away_games(team_id)
+    @game_teams.find_all do |game_team|
+      game_team.hoa == "away" && game_team.team_id == team_id
+    end
+  end
+
+  def home_win_percentage(team_id)
+    home_wins = home_games(team_id).find_all do |game|
+      game.results == "WIN"
+    end
+    home_wins/home_games(team_id).length
+  end
+
+  def away_win_percentage(team_id)
+    away_wins = away_games(team_id).find_all do |game|
+      game.results == "WIN"
+    end
+    home_wins/home_games(team_id).length
+  end
+
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -20,10 +20,48 @@ class StatTrackerTest < Minitest::Test
   def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
   end
-  
+
   def test_it_can_create_objects
     assert_instance_of Game, @stat_tracker.games[2]
     assert_instance_of Team, @stat_tracker.teams[2]
     assert_instance_of GameTeam, @stat_tracker.game_teams[2]
   end
+
+
+
+  def test_finding_home_games_for_a_team
+    assert_equal 5, @stat_tracker.home_games(6).length
+    # add another assert
+  end
+
+  def test_finding_away_games_for_a_team
+    assert_equal 4, @stat_tracker.away_games(6).length
+  end
+
+  def test_getting_home_win_percentage_for_a_team
+    skip
+    assert_equal 15, @stat_tracker.home_win_percentage(6)
+  end
+
+  def test_getting_away_win_percentage_for_a_team
+    skip
+    assert_equal 40, @stat_tracker.away_win_percentage(6)
+  end
+
+  def test_getting_the_team_with_the_best_fans
+    skip
+    assert_equal 6, @stat_tracker.best_fans(6)
+  end
+
+  # Name of the team with biggest difference between
+  # home and away win percentages.
+
+  def test_getting_the_teams_with_the_worst_fans
+    skip
+  end
+
+  # List of names of all teams with better away records than home records.
+
+
+
 end
